bab6d0ef3b61656e7ea0723413016fc5860aa169
approvedweb2025 <approvedweb2025@gmail.com>
2025-09-19 23:58:23 +0500

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { UserProvider } from './Context/UserContext.jsx';
 import { MapProvider } from './Context/MapContext.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <GoogleOAuthProvider clientId="1095880633623-6qk34obmap23cta2vrhoim29bfmi6tg9.apps.googleusercontent.com">
+  <GoogleOAuthProvider clientId="1095880633623-5igvfdifks2r47a3o2ao6ljkj02juonj.apps.googleusercontent.com">
 
     <MapProvider>
       <UserProvider>
